3f6e2218e25255b009d6c5cc7b2e124f002beee2
Egor <sartvas@yandex.ru>
2021-02-28 22:41:01 +0300

    add first JS

---
+++ js/firstJS.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 11;
+//console.log(number);
+
+// const obj = {
+//    name: "Valera",
+//    surname: 'Pirtgtt',
+//    age: 18,
+//    isMarried: false
+//};/
+
+//console.log(obj.name);
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// alert('hello!');
+
+//const result = confirm("Are you foggot?");
+//console.log(result);
+
+//const answer = prompt("Are yoy 18 years old?:","");
+//console.log(answer);
+
+//const answers = [];
+
+//answers[0] = prompt("What is your name?:", "");
+//answers[1] = prompt("What is your surname?:", "");
+//answers[3] = prompt("Hoe old are you?:", "");
+
+//document.write(answers);
+
+//let names = prompt("Write your name, please!:","");
+//console.log(`Hi there, ${names}`);
