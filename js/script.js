330ceeaeecf3344ac7b4fba1962081b179d599b1
Egor <sartvas@yandex.ru>
2021-03-01 06:54:50 +0300

    start practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+const numbersOfFilms = prompt("How many fims you see?:", "");
+
+const personalMovieBD = {
+    count:numbersOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat:false,
+};
+
+const a = prompt('What is your last film`s see?', ''),
+      b = prompt('How you rate this films?', ''),
+      c = prompt('What is your last film`s see?', ''),
+      d = prompt('How you rate this films?', '');
+
+personalMovieBD.movies[a] = b;
+personalMovieBD.movies[c] = d;
+
+console.log(personalMovieBD);
